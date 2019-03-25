44d5f94c4a1195c657cf0336d36a292ece8ee76e
Lexouden <99044652@mydavinci.nl>
2019-03-25 14:57:10 +0100

    Route and removed newitem and newlist view

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Todo List' });
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title: 'Todo List'
+  });
+});
+
+router.get('/newlist', (req, res, next) => {
+  res.render('newlist', {
+    title: 'Make a new list'
+  });
+});
+
+router.get('/newitem', (req, res, next) => {
+  res.render('newitem', {
+    title: 'Make a new item'
+  });
 });
 
 module.exports = router;
