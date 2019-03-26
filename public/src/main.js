d3996ed8638ddeb676ddf083783f39585a6411bf
Lexouden <99044652@mydavinci.nl>
2019-03-26 18:18:24 +0100

    Added list creation

--- public/src/main.js
+++ public/src/main.js
@@ -15,6 +15,11 @@ import {
 
 // Get list container
 const lists = document.getElementById('lists');
+$('#newItemBtn').click(createNewItem);
+$('#newListBtn').click(createNewList);
+$('#listModalSave').click(saveList);
+
+$('#myModal').modal('handleUpdate')
 
 // Print / Write results to screen
 const writeToScreen = async (list) => {
@@ -31,6 +36,13 @@ const writeToScreen = async (list) => {
   const title = document.createElement('h3');
   title.innerText = list.name;
   listElement.appendChild(title);
+
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-danger', 'float-right', 'fa', 'fa-times')
+  button.addEventListener('click', removeList);
+  button.setAttribute('data-list-id', list.id);
+  listElement.appendChild(button);
+
 
   // Create sortable table
   const table = document.createElement('table');
@@ -60,7 +72,7 @@ const writeToScreen = async (list) => {
       case 2:
         th.innerText = 'Status';
         break;
-      case 3: 
+      case 3:
         th.innerText = '';
         th.classList.add('sorttable_nosort');
         break;
@@ -91,13 +103,13 @@ const writeToScreen = async (list) => {
         case 2:
           td.innerText = item.status;
           break;
-        case 3: 
+        case 3:
           td.appendChild(button);
           button.classList.add('btn', 'btn-danger', 'far', 'fa-times-circle', 'removebtn');
           button.addEventListener('click', removeItem);
           button.setAttribute('data-item-id', key)
           button.setAttribute('data-list-id', list.id)
-          break;  
+          break;
         default:
           console.error('An unexpected error occured');
       }
@@ -138,18 +150,61 @@ export async function updateScreen() {
   listClass.remove(); // Remove list elements from DOM
 
   // Re-render lists
-  try{  
+  try {
     var newLists = await getLists();
     newLists.forEach(list => {
       writeToScreen(list);
     });
-  }catch(e){
+  } catch (e) {
     console.log(e.stack);
   }
 }
 
 // Functions for item management
-function createNewItem() {
+async function createNewItem() {
+  let label = $('#itemModalLabel');
+  let body = $('#itemModalBody');
+
+  let template = `<form class="m-2">
+  <div class="form-group row">
+    <label for="description">Description</label>
+    <textarea class="form-control" id="description" cols="30" rows="3" required></textarea>
+  </div>
+  <div class="form-group row">
+    <div class="form-group">
+      <label for="duration">Duration</label>
+      <input class="form-control" type="number">
+    </div>
+    <div class="form-group form-check">
+      <label for="status">Status</label>
+      <select id="status" class="form-control" required>
+        <option value="Unfinished" selected>Unfinished</option>
+        <option value="Finished">Finished</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="listSelect">List</label>
+    <select class="form-control" id="listSelect" required>
+      <option disabled selected>Select a list to assign the item to.</option>
+    </select>
+  </div>
+</form>`;
+
+  $('#itemModal').modal('toggle');
+
+  label.text('Create new item');
+  body.html(template);
+
+  // Add all lists to select field
+  let lists = await getLists();
+  for (let list in lists) {
+    $('#listSelect').append(`<option value=${lists[list].id}>${lists[list].name}</option>`)
+  }
+
+}
+
+function saveItem() {
 
 }
 
@@ -158,21 +213,69 @@ function updateItem() {
 }
 
 async function removeItem() {
-  if(!this.dataset.itemId && !this.dataset.listId) return console.error('Missing data attribute')
+  if (!this.dataset.itemId && !this.dataset.listId) return console.error('Missing data attribute')
   await deleteItem(this.dataset.itemId, parseInt(this.dataset.listId));
 }
 
 // Functions for list management 
 function createNewList() {
+  let label = $('#listModalLabel');
+  let body = $('#listModalBody');
+  let save = $('#listModalSave');
 
+  let template = `<form class="m-2">
+              <div class="form-group row">
+                <label for="name">Name</label>
+                <input class="form-control" type="text" id="name" autocomplete="off" required>
+              </div>
+            </form>`
+
+  $('#listModal').modal('toggle');
+
+  label.text('Create new List');
+  body.html(template);
+  save.data('type', 'new');
 }
 
-function updateList(list) {
+function saveList() {
+  let save = $('#listModalSave');
+  let type = save.data('type');
 
+  if (type === 'new') {
+    let name = $('#name').val();
+    if (!name || name === '') return; // Check if name is given
+    $('#listModal').modal('toggle'); // Close Modal
+
+    return newList(name);
+  }
 }
 
-function removeList(list) {
+async function updateList() {
+  let label = $('#listModalLabel');
+  let body = $('#listModalBody');
+  let save = $('#listModalSave');
+  let name = $(this).data('list-name');
+  let id = $(this).data('list-id');
 
+  let template = `<form class="m-2">
+              <div class="form-group row">
+                <label for="name">Name</label>
+                <input class="form-control" type="text" id="name" autocomplete="off" value='${name}' required>
+              </div>
+            </form>`;
+
+  $('#listModal').modal('toggle');
+
+  label.text(`Change the name of ${name} list`);
+  body.html(template);
+  save.data('type', 'update');
+  save.data('parameter-id', id);
+}
+
+async function removeList() {
+  if (!this.dataset.listId) return console.error('List id not received from data attribute');
+
+  await deleteList(parseInt(this.dataset.listId));
 }
 
 /**
