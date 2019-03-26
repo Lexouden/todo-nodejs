cead264c99c668610b931fae13306a961e3e83c1
Lexouden <99044652@mydavinci.nl>
2019-03-26 13:19:00 +0100

    Changed from jade templating to plain HTML

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.clear();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -10,13 +11,14 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static('./public',{extensions: ['html']}));
 
 app.use('/', indexRouter);
 
